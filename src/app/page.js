0568c0627f2d3470a0c91b4544457775552db103
Muhammad Junaid <junaidmughal017786@gmail.com>
2026-02-21 12:18:13 +0500

    docs: Setup reel-02-beamgrid demo branch

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import { BentoShowcase } from "@/components/Bento/BentoShowcase";
 import BeamGridDemo from "@/components/BeamGrid/BeamGridDemo";
 export default function Home() {
   return (
     <>
-      <BentoShowcase />
       <BeamGridDemo />
     </>
   );
