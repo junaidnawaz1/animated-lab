1a234452a2d770b30c2de51c1e2f967e100d58cb
Muhammad Junaid <junaidmughal017786@gmail.com>
2026-02-25 12:22:59 +0500

    Reel-03: AnimeNavBar component only

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
 import Image from "next/image";
-import { BentoShowcase } from "@/components/Bento/BentoShowcase";
-import BeamGridDemo from "@/components/BeamGrid/BeamGridDemo";
 import { AnimeNavBarDemo } from "@/components/AnimeNavBar/Demo";
 export default function Home() {
   return (
     <>
-      <BentoShowcase />
-      <BeamGridDemo />
       <AnimeNavBarDemo/>
     </>
   );
