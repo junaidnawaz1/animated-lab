ee2b98c919905cc547cc4856d6f9cb48a8d97e44
Muhammad Junaid <junaidmughal017786@gmail.com>
2026-03-03 20:11:36 +0500

    Happy Coding - Reel 05 Sliding Auth Form

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
-import Image from "next/image";
-import { BentoShowcase } from "@/components/Bento/BentoShowcase";
-import BeamGridDemo from "@/components/BeamGrid/BeamGridDemo";
-import { AnimeNavBarDemo } from "@/components/AnimeNavBar/Demo";
-import  ProfileCard  from "@/components/ProfileCard/ProfileCard";
+
 import AuthSwitchDemo from "@/components/AnimatedForm/AuthSwitchDemo";
 export default function Home() {
   return (
     <>
-    
-      <ProfileCard/>
-      <AuthSwitchDemo/>
+          <AuthSwitchDemo/>
     </>
   );
 }
